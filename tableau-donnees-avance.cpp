8f5fb5c5306be5af5abf9e43e2dbf1891d9b3db4
Walidb200 <walidbouzid95870@gmail.com>
2024-12-08 10:22:17 +0100

    Update tableau-donnees-avance.cpp

--- tableau-donnees-avance.cpp
+++ tableau-donnees-avance.cpp
@@ -58,14 +58,19 @@ vector<int> groupByInt(vector<vector<string>> data, vector<string> valeurs, int 
 }
 
 vector<double> groupByDouble(vector<vector<string>> data, vector<string> valeurs, int j1, int j2) {
-    vector<double> resultat(valeursDistinctes.size(), 0.0);
-    for (const auto& ligne : data) {
-        string valeurj1 = ligne[j1];
-        double valeurj2 = conversionDouble(ligne[j2]);
-        int index = chercheIndice(valeurs, valeurj1);
-        if (index != -1) {
-            resultat[index] += valeurj2;
+    vector<double> resultat(valeurs.size());
+    for (int i = 0; i <valeurs.size(); i++) {
+        string valeur_s = valeurs[i];
+        for (int j = 0; j<data.size(); j++) {
+            if(data[j][j1] == valeur_s) {
+                stringstream ss;
+                ss << data[j][j2];
+                double res;
+                ss >> res;
+                resultat[i] += res;
+            
         }
+    }
     }
     return resultat;
 }
