82f462774fd63ffe585fe17ed08bcfd85157efd6
Walidb200 <walidbouzid95870@gmail.com>
2024-12-10 22:40:37 +0100

    Update arbres-hauteur.cpp

--- arbres-hauteur.cpp
+++ arbres-hauteur.cpp
@@ -1,37 +1,48 @@
 #include <stdexcept>
 /** @file **/
 #include <iostream>
+#include <sstream>
+#include <string>
 #include "tableau-donnees.hpp"
 #include "tableau-lecture-csv.hpp"
 using namespace std;
+
 
 /** Affiche le genre et l'espece de l'arbre le plus haut de Paris
  * parmi les "arbres remarquables"
  **/
 
 
+
+
+
 int main() {
-    string fichier_nom = "arbres_remarquables.csv"; 
-    int nb_colonnes = 4; 
-    vector<vector<string>> arbres = litTableauCSV(fichier_nom, nb_colonnes);
+    vector<vector<string>> arbres = litTableauCSV("donnees/arbresremarquablesparis.csv");
+    vector<int> hauteur = conversionInt(colonne(litTableauCSV("donnees/arbresremarquablesparis.csv"), 8));
+    for(int i = 0; i<hauteur.size(); i++) {
+        cout << hauteur[i] <<endl;
+    }
     double max_hauteur = 0.0;
     vector<string> arbre_plus_haut;
+    string libelle_fr;
+    string genre;
+    string espece;
+    for (int i = 0; i < hauteur.size(); i++) {
+        int haut = hauteur[i];
 
-    for (size_t i = 0; i < arbres.size(); i++) {
-        vector<string>& ligne = arbres[i];
-        double hauteur = stod(ligne[3]); 
-
-        if (hauteur > max_hauteur) {
-            max_hauteur = hauteur;
-            arbre_plus_haut = ligne;
+        if (haut > max_hauteur) {
+            max_hauteur = haut;
+            libelle_fr = arbres[i][15];
+            genre = arbres[i][11];
+            espece = arbres[i][12];
+            //arbre_plus_haut = ligne;
         }
     }
-    cout << "L'arbre le plus haut de Paris :\n";
-    cout << "Nom français : " << arbre_plus_haut[0] << "\n";
-    cout << "Genre : " << arbre_plus_haut[1] << "\n";
-    cout << "Espèce : " << arbre_plus_haut[2] << "\n";
-    cout << "Hauteur : " << max_hauteur << " m\n";
+    cout << "L'arbre le plus haut de Paris : " << endl;
+    cout << "Libellé français : " << libelle_fr << endl;
+    cout << "Genre : " << genre  << endl;
+    cout << "Espèce : " << espece << endl;
+    cout << "Hauteur : " << max_hauteur << "m" << endl;
 
     return 0;
 }
-
