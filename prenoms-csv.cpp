b255d1caf1707ec33ce31bafda759317991e3281
Walidb200 <walidbouzid95870@gmail.com>
2024-12-10 14:03:41 +0100

    Update prenoms-csv.cpp

--- prenoms-csv.cpp
+++ prenoms-csv.cpp
@@ -6,27 +6,40 @@
 #include <sstream>
 using namespace std;
 
+
+
 /** Calcule et affiche le prénom le plus donné une année donnée
  * ainsi que le nombre de naissance cette année là **/
 int main() {
-    ifstream flux;
-    flux.open("liste_des_prenoms.csv");
+    ifstream flux("donnees/liste_des_prenoms.csv");
     string ligne;
-    char s;
-    int a;
-    string p, pmax;
-    int n;
-    int resultat, maxn;
-    int a_propose;
+    getline(flux, ligne);
+    string nombre1;
+    string MF;
+    string annee;
+    string prenoms;
+    string nombre2;
+    int resultat = 0;
+    int nombre;
+    int a_propose = 0;
+    int maxn = 0;
+    string pmax = "";
+    int annee_int;
     cout << "Entrez une année entre 2006 et 2021 : ";
     cin >> a_propose;
-    while(getline(flux,ligne)) {
-        flux >> s >> a >> p >> n;
-        if(a_propose == a) {
-            resultat += n;
-            if(maxn < n) {
-                maxn = n;
-                pmax = p;
+    while(flux) {
+        getline(flux, nombre1, ';');
+        getline(flux, MF, ';');
+        getline(flux, annee, ';');
+        getline(flux, prenoms, ';');
+        getline(flux, nombre2);
+        istringstream(nombre2) >> nombre;
+        stringstream(annee) >> annee_int;
+        if(a_propose == annee_int) {
+            resultat += nombre;
+            if(maxn < nombre) {
+                maxn = nombre;
+                pmax = prenoms;
             }
         }
     }
@@ -35,4 +48,3 @@ int main() {
     flux.close();
     return 0;
 }
-
